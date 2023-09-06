9482e6ab99996202b31aad957b6ac28876ad755e
MingZuoX <zuoming012@gmail.com>
2023-09-06 16:33:50 +0800

    Update dns_info.py

--- dns_info.py
+++ dns_info.py
@@ -1,2 +1,12 @@
-import dnspython
+import dns
+from dns import resolver
 
+
+def get_dns_info(url_input):
+    result = resolver.resolve(url_input, 'MX')
+    for rdata in result:
+        # print('IP', ipval.to_text())
+        print('Host', rdata.exchange, 'has preference', rdata.preference)
+
+
+get_dns_info('baidu.com')
